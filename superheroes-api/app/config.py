ad6ede1a8b130db801de9a64eedb99b227a7b683
Brandon2401 <dikirrbrandon@gmail.com>
2026-01-13 10:35:15 +0300

    did the apps configuration

---
+++ superheroes-api/app/config.py
@@ -0,0 +1,9 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = "sqlite:///superheroes.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = "your_email@gmail.com"
+    MAIL_PASSWORD = "your_app_password"
